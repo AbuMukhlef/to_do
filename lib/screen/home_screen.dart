ba5543464964fa3b0ed5f481a0d0364234881a4d
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-09-08 06:59:01 +0300

    ADD showModalBottomSheet

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:getit_with_bloc/core/extension/get_screen_size.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,18 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(title: const Text('Day Task')),
       body: const Center(
         child: Text('Home'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) => Container(
+                  color: Colors.amber,
+                  height: context.getWidthScreen(multipleWidth: 0.9),
+                  width: double.infinity,
+                  child: const Text('Hello')));
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
